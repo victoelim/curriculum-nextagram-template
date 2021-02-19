f8adb1aedb09fb64661791bbf31d75fc7cfd92ac
Victor Lim <vlxa@outlook.com>
2021-02-19 23:45:34 +0800

    added csrf

--- phonebook_api/__init__.py
+++ phonebook_api/__init__.py
@@ -6,4 +6,6 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 ## API Routes ##
 from phonebook_api.blueprints.users.views import users_api_blueprint
 
+csrf.exempt(users_api_blueprint)
+
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
